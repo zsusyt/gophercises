7d11855a6d6155c874bccb757112b7f1d4565512
zsusyt <17499032@qq.com>
2019-02-06 14:59:13 +0800

    e9

--- e9/card_test.go
+++ e9/card_test.go
@@ -1,6 +1,10 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
 
 func ExampleCard () {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
@@ -16,3 +20,93 @@ func ExampleCard () {
 	// Jack of Clubs
 	// Joker
 }
+
+func TestNew(t *testing.T) {
+	cards := New ()
+	if len(cards) != 13*4 {
+		t.Error("Wrong number of cards")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+	}
+}
+
+func TestSort(t *testing.T) {
+	cards := New(Sort(Less))
+	exp := Card{Rank: Ace, Suit: Spade}
+	if cards[0] != exp {
+		t.Error("Expected Ace of Spades as first card. Received:", cards[0])
+	}
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+	for _, c := range cards {
+		if c.Suit == Joker {
+			count ++
+		}
+	}
+	if count != 3 {
+		t.Error("Expected 3 Jokers, received:", count)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	filter := func (card Card) bool {
+		return card.Rank == Two || card.Rank == Three
+	}
+	cards := New(Filter(filter))
+	for _, c := range cards {
+		if c.Rank == Two || c.Rank == Three {
+			t.Error("Expected all twos and threes to be filtered out.")
+		}
+	}
+}
+
+func TestDeck(t *testing.T) {
+	cards := New(Deck(3))
+	if len(cards) != 13*4*3 {
+		t.Errorf("Expected %d cards, received %d cards.", 13*4*3, len(cards))
+	}
+}
+
+func TestShuffle(t *testing.T) {
+	shuffleRand = rand.New(rand.NewSource(0))
+	orig := New()
+	first := orig[40]
+	second := orig[35]
+	cards := New(Shuffle)
+	if cards[0] != first {
+		t.Errorf("Expected the first card to be %s, received %s", first, cards[0])
+	}
+	if cards[1] != second {
+		t.Errorf("Expected the Second card to be %s, received %s", second, cards[1])
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
