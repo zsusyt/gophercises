6e579f0a6425bce7b70b5adb1c90e976315b071c
zsusyt <17499032@qq.com>
2019-02-17 01:16:16 +0800

    ex11 finished

--- e11/ai.go
+++ e11/ai.go
@@ -18,7 +18,7 @@ func (ai dealerAI) Bet(shuffled bool) int {
 	return 0
 }
 
-func (ai dealerAI) Play(hand []deck.Card, dealder deck.Card) Move {
+func (ai dealerAI) Play(hand []deck.Card, dealer deck.Card) Move {
 	dScore := Score(hand...)
 	if dScore < 16 || (dScore == 17 && Soft(hand...)) {
 		return MoveHit
