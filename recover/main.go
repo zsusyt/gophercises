9df8856f4ecc1335e3a0ee9ba7171ec8a3f34d1f
zsusyt <17499032@qq.com>
2019-02-25 18:10:48 +0800

    ex14

---
+++ recover/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+"fmt"
+"log"
+"net/http"
+	"runtime/debug"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/panic/", panicDemo)
+	mux.HandleFunc("/panic-after/", panicAfterDemo)
+	mux.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
+}
+func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				stack := debug.Stack()
+				log.Println(string(stack))
+				if !dev {
+					http.Error(w, "Something went wrong :(", http.StatusInternalServerError)
+					return
+				}
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err,string(stack))
+			}
+		}()
+
+		nw := &responseWriter{ResponseWriter: w}
+		app.ServeHTTP(nw, r)
+		nw.flush()
+	}
+}
+
+type responseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *responseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func panicDemo(w http.ResponseWriter, r *http.Request) {
+	funcThatPanics()
+}
+
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello!</h1>")
+	funcThatPanics()
+}
+
+func funcThatPanics() {
+	panic("Oh no!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
